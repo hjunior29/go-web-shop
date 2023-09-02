83499f6d6f2601897dd926d0a29518679b5132e2
hjunior29 <helderjuniorsilvalima@gmail.com>
2023-09-02 19:01:27 -0300

    feat: add crud

---
+++ controllers/produtos.go
@@ -0,0 +1,79 @@
+package controllers
+
+import (
+	"Web/models"
+	"log"
+	"net/http"
+	"strconv"
+	"text/template"
+)
+
+var temp = template.Must(template.ParseGlob("templates/*html"))
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	produtos := models.BuscaProdutos()
+	temp.ExecuteTemplate(w, "Index", produtos)
+}
+
+func New(w http.ResponseWriter, r *http.Request)  {
+	temp.ExecuteTemplate(w, "New", nil)
+}
+
+func Insert(w http.ResponseWriter, r *http.Request)  {
+	if r.Method == "POST" {
+		nome := r.FormValue("nome")
+		descricao := r.FormValue("descricao")
+		preco := r.FormValue("preco")
+		quantidade := r.FormValue("quantidade")
+
+		precoConvertido, err := strconv.ParseFloat(preco, 64)
+		if err != nil {
+			log.Println(err)
+		}
+
+		quantidadeConvertido, err := strconv.Atoi(quantidade)
+		if err != nil {
+			log.Println(err)
+		}
+
+		models.CriaNovoProduto(nome, descricao, precoConvertido, quantidadeConvertido)
+	}
+	http.Redirect(w,r, "/", 301)
+}
+
+func Delete(w http.ResponseWriter, r *http.Request)  {
+	idProduto := r.URL.Query().Get("id")
+	models.DeletaProduto(idProduto)
+	http.Redirect(w,r, "/", 301)
+}
+
+func Edit(w http.ResponseWriter, r *http.Request)  {
+	idProduto := r.URL.Query().Get("id")
+	produto := models.EditaProduto(idProduto)
+	temp.ExecuteTemplate(w, "Edit", produto)
+}
+
+func Update(w http.ResponseWriter, r *http.Request)  {
+	if r.Method == "POST" {
+		id := r.FormValue("id")
+		nome := r.FormValue("nome")
+		descricao := r.FormValue("descricao")
+		preco := r.FormValue("preco")
+		quantidade := r.FormValue("quantidade")
+	
+		idConvertido, err := strconv.Atoi(id)
+		if err != nil {
+			log.Println(err)
+		}
+		precoConvertido, err := strconv.ParseFloat(preco, 64)
+		if err != nil {
+			log.Println(err)
+		}
+		quantidadeConvertido, err := strconv.Atoi(quantidade)
+		if err != nil {
+			log.Println(err)
+		}
+		models.AtualizaProduto(idConvertido, nome, descricao, precoConvertido, quantidadeConvertido)
+	}
+	http.Redirect(w,r, "/", 301)
+}
